e05cac92237e8b083c5be5200add4b305f6b65a6
Alfonso <vc.alfonso@hotmail.com>
2020-08-21 15:58:41 -0500

    calendar todo app

---
+++ calendar_app/src/app/components/CalendarWeek.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="calendaer-week" class="container">
+    <div class="columns is-mobile">
+      <CalendarDay v-for="day in sharedStare.seedData" :key="day.id" :day="day" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { store } from '../store.js';
+import CalendarDay from './CalendarDay.vue';
+
+export default {
+  name: 'CalendarWeek',
+  data() {
+    return {
+      sharedStare: store.state,
+    };
+  },
+  components: {
+    CalendarDay,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#calendar-week {
+  margin-bottom: 50px;
+  .column {
+    padding: 0px;
+  }
+}
+</style>
